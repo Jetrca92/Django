74c3b500a17ad222dbbdac8918ad3440ec0bc063
Jetrca92 <blaz.guzej@gmail.com>
2022-11-18 08:26:52 +0100

    Make models

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,16 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class AuctionListings(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.CharField(max_length=256)
+    category = models.CharField(max_length=64)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+
+class Bids(models.Model):
+    starting_bid = models.DecimalField(decimal_places=2, max_digits=10)
+
+
+class Comments(models.Model):
+    comment = models.CharField(max_length=128)
