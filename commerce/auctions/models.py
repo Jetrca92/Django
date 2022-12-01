bc1fc71f1f1dcb6e7ed4ecbe1adfdf6934607eee
Jetrca92 <blaz.guzej@gmail.com>
2022-12-01 12:18:06 +0100

    Add noimage value

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -15,7 +15,7 @@ class Listing(models.Model):
     name = models.CharField(max_length=64)
     description = models.CharField(max_length=256)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True, related_name="category")
-    imgurl = models.CharField(max_length=512)
+    imgurl = models.CharField(max_length=512, default="https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg")
     price = models.DecimalField(decimal_places=2, max_digits=10)
     is_active = models.BooleanField(default=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, related_name="user")
