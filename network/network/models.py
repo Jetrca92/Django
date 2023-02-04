f821249c23018df2aa73f6deaa082ba079e0a1fa
Jetrca92 <blaz.guzej@gmail.com>
2023-02-04 14:43:56 +0100

    Create model Post and migrate

--- network/network/models.py
+++ network/network/models.py
@@ -4,3 +4,9 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, related_name="author")
+    content = models.CharField(max_length=280)
+    date = models.DateTimeField(auto_now_add=True)
+    likes = models.IntegerField(default=0)
