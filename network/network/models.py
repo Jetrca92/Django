efa96174681ad39ab5fb3b8170917d7e96f01a7a
Jetrca92 <blaz.guzej@gmail.com>
2023-02-05 08:45:40 +0100

    Edit and migrate models

--- network/network/models.py
+++ network/network/models.py
@@ -16,8 +16,8 @@ class Post(models.Model):
 
 class Profile(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, related_name="owner")
-    followers = models.ManyToManyField(User, blank=True, null=True, related_name="followers")
-    following = models.ManyToManyField(User, blank=True, null=True, related_name="following")
+    followers = models.ManyToManyField(User, blank=True, related_name="followers")
+    following = models.ManyToManyField(User, blank=True, related_name="following")
 
     def __str__(self):
         return self.owner.username
